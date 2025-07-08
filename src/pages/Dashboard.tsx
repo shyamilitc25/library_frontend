2124bb024b7fb8d25528ac2201e8707b48589cd9
shyamilitc25 <shyamilitc@gmail.com>
2025-07-08 16:07:19 +0530

    new changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -14,7 +14,7 @@ function Dashboard() {
       <h1>My App</h1>
       <input onChange={(e) => { name = e.target.value }} />
       <button onClick={handleClick}>Click</button>
-      {clicked && <p>You clicked! review this code on push attempt 3</p>}
+      {clicked && <p>You clicked! review this code on push attempt 4 and nit working</p>}
     </div>
   );
 }
